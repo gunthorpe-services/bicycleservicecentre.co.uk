e15059c79c96aceddea64f601e468d62ab9cdaf2
David Gunthorpe <mail@davidgunthorpe.co.uk>
2020-03-06 13:09:27 +0000

    react version of website

---
+++ precache-manifest.8e31a4db108d2eb88de113277f02b4a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bde11e0230a09bcba8f5ab589cbc7b01",
+    "url": "/bicycleservicecentre.co.uk/index.html"
+  },
+  {
+    "revision": "d6664a900448f2174dba",
+    "url": "/bicycleservicecentre.co.uk/static/css/2.9b46dffb.chunk.css"
+  },
+  {
+    "revision": "ae1087ef7144fe8a1992",
+    "url": "/bicycleservicecentre.co.uk/static/css/main.7381aeb1.chunk.css"
+  },
+  {
+    "revision": "d6664a900448f2174dba",
+    "url": "/bicycleservicecentre.co.uk/static/js/2.43434fdb.chunk.js"
+  },
+  {
+    "revision": "ed5538538bc679444f05674aa3bb2226",
+    "url": "/bicycleservicecentre.co.uk/static/js/2.43434fdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1087ef7144fe8a1992",
+    "url": "/bicycleservicecentre.co.uk/static/js/main.5b649a0e.chunk.js"
+  },
+  {
+    "revision": "1edf724a4138e87d5e60",
+    "url": "/bicycleservicecentre.co.uk/static/js/runtime-main.2c28de6c.js"
+  }
+]);
